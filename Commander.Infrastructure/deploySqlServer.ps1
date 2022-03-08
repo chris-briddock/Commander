ae8f821f11c5c4d15545157fde16ad4c0eb618e0
Christopher Briddock <Christopher.Briddock@opengi.co.uk>
2022-03-08 05:34:40 +0000

    Added deploySqlServer.ps1

---
+++ Commander.Infrastructure/deploySqlServer.ps1
@@ -0,0 +1,5 @@
+az sql server create -l westus3 -g commander-rg -n commanderServer-01 -u chris -p "*gj8~jUZD@!/u]:^j)~!(9@!2nYwwQeaGu2G/mDA8^G2>*)7V:tk$%@m[Bsm*jVBh)T!.kU.FxYnq<;PB~J2T83+Ps6==o:WT#"
+az sql server create -l eastus -g commander-rg -n commanderServer-02 -u chris -p "*gj8~jUZD@!/u]:^j)~!(9@!2nYwwQeaGu2G/mDA8^G2>*)7V:tk$%@m[Bsm*jVBh)T!.kU.FxYnq<;PB~J2T83+Ps6==o:WT#"
+az sql db create -g commander-rg -s commanderServer-01 -n Commander --service-objective S0
+az sql db create -g commander-rg -s commanderServer-02 -n Commander --service-objective S0
+az sql failover-group create --name commanderFailover --partner-server commanderServer-02 --resource-group commander-rg --server commanderServer-01
