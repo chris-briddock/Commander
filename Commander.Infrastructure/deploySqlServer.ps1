661307d805706224e882f9b9ee0424b9ba195f54
Christopher Briddock <Christopher.Briddock@opengi.co.uk>
2022-03-08 07:11:04 +0000

    Updated deploySqlServer.ps1

--- Commander.Infrastructure/deploySqlServer.ps1
+++ Commander.Infrastructure/deploySqlServer.ps1
@@ -1,4 +1,4 @@
-az login
+az login --use-device-code
 az sql server create -l westus3 -g commander-rg -n commanderServer-01 -u chris -p "*gj8~jUZD@!/u]:^j)~!(9@!2nYwwQeaGu2G/mDA8^G2>*)7V:tk$%@m[Bsm*jVBh)T!.kU.FxYnq<;PB~J2T83+Ps6==o:WT#"
 az sql server create -l eastus -g commander-rg -n commanderServer-02 -u chris -p "*gj8~jUZD@!/u]:^j)~!(9@!2nYwwQeaGu2G/mDA8^G2>*)7V:tk$%@m[Bsm*jVBh)T!.kU.FxYnq<;PB~J2T83+Ps6==o:WT#"
 az sql db create -g commander-rg -s commanderServer-01 -n Commander --service-objective S0
