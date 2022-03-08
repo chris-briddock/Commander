e94315c461ca6063effed0569bbb2b425a060397
Chris Briddock <christopher.briddock@opengi.co.uk>
2022-03-08 08:10:23 +0000

    i c u

--- Commander.Infrastructure/deploySqlServer.ps1
+++ Commander.Infrastructure/deploySqlServer.ps1
@@ -1,7 +1,7 @@
 az login --use-device-code
-az group create -l westus3 -n commander-rg 
-az sql server create -l westus3 -g commander-rg -n commanderServer-01 -u chris -p $($env:Password)
-az sql server create -l eastus -g commander-rg -n commanderServer-02 -u chris -p $($env:Password)
-az sql db create -g commander-rg -s commanderServer-01 -n Commander --service-objective S0
-az sql db create -g commander-rg -s commanderServer-02 -n Commander --service-objective S0
-az sql failover-group create --name commanderFailover --partner-server commanderServer-02 --resource-group commander-rg --server commanderServer-01
+az group create -l westus3 -n commander-sql-rg
+az sql server create -l westus3 -g commander-sql-rg -n commanderServer-01 -u chris -p $($env:Password)
+az sql server create -l eastus -g commander--sql-rg -n commanderServer-02 -u chris -p $($env:Password)
+az sql db create -g commander-sql-rg -s commanderServer-01 -n Commander --service-objective S0
+az sql db create -g commander-sql-rg -s commanderServer-02 -n Commander --service-objective S0
+az sql failover-group create --name commanderFailover --partner-server commanderServer-02 --resource-group commander-sql-rg --server commanderServer-01
