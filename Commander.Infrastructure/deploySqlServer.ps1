4bdb3335421ffda5ea14654e5d51748208b4c6ba
Chris Briddock <christopher.briddock@opengi.co.uk>
2022-03-08 07:30:23 +0000

    Changed to env variable for pipelines

--- Commander.Infrastructure/deploySqlServer.ps1
+++ Commander.Infrastructure/deploySqlServer.ps1
@@ -1,5 +1,5 @@
 az login --use-device-code
-az sql server create -l westus3 -g commander-rg -n commanderServer-01 -u chris -p $(Password)
+az sql server create -l westus3 -g commander-rg -n commanderServer-01 -u chris -p $($env:Password)
 az sql server create -l eastus -g commander-rg -n commanderServer-02 -u chris -p $(Password)
 az sql db create -g commander-rg -s commanderServer-01 -n Commander --service-objective S0
 az sql db create -g commander-rg -s commanderServer-02 -n Commander --service-objective S0
